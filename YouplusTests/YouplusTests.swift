d6dfa12daf4fbed95381a1636b011352e3d12ca4
Andrew Fruth <anfruth@gmail.com>
2016-08-18 14:36:12 -0700

    Initial Commit

---
+++ YouplusTests/YouplusTests.swift
@@ -0,0 +1,36 @@
+//
+//  YouplusTests.swift
+//  YouplusTests
+//
+//  Created by Andrew Fruth on 8/18/16.
+//  Copyright © 2016 Huge American Corporation. All rights reserved.
+//
+
+import XCTest
+@testable import Youplus
+
+class YouplusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
